58ac6e38378e28c25b6fa7dcd10f41b2017c2d6d
Gustavo de Vito <gustavodevito38@gmail.com>
2023-02-08 22:01:48 -0300

    lib: add lib ui

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,14 @@
 import { Inter } from '@next/font/google'
+import { NextUIProvider } from '@nextui-org/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }) {
   return (
-    <main className={inter.className}>
-      <Component {...pageProps} />
-    </main>
+    <NextUIProvider>
+      <main className={inter.className}>
+        <Component {...pageProps} />
+      </main>
+    </NextUIProvider>
   );
 }
