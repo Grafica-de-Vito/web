7600160642c28b93682b5db1bace417aee8198a6
Gustavo de Vito <gustavodevito38@gmail.com>
2023-02-09 22:59:37 -0300

    feat: add navbar

--- src/components/Nabvar.js
+++ src/components/Nabvar.js
@@ -1,7 +1,66 @@
-const Navbar = () => {
+import { Navbar, Button, Link, Text } from "@nextui-org/react";
+import Layout from "./Layout";
+import { Logo } from "@/icons";
+
+const NavbarUI = () => {
+    const collapseItems = [
+        "Features",
+        "Customers",
+        "Pricing",
+        "Company",
+        "Legal",
+        "Team",
+        "Help & Feedback",
+        "Login",
+        "Sign Up",
+    ];
+
     return (
-        <>Navbar</>
+        <Layout>
+            <Navbar isBordered variant="sticky">
+                <Navbar.Brand>
+                    <Navbar.Toggle aria-label="toggle navigation" />
+                    <Logo />
+                    <Text b color="inherit" hideIn="xs">
+                        ACME
+                    </Text>
+                </Navbar.Brand>
+                <Navbar.Content enableCursorHighlight hideIn="xs" variant="underline">
+                    <Navbar.Link href="#">Features</Navbar.Link>
+                    <Navbar.Link isActive href="#">
+                        Customers
+                    </Navbar.Link>
+                    <Navbar.Link href="#">Pricing</Navbar.Link>
+                    <Navbar.Link href="#">Company</Navbar.Link>
+                </Navbar.Content>
+                <Navbar.Content>
+                    <Navbar.Link color="inherit" href="#">
+                        Login
+                    </Navbar.Link>
+                    <Navbar.Item>
+                        <Button auto flat as={Link} href="#">
+                            Sign Up
+                        </Button>
+                    </Navbar.Item>
+                </Navbar.Content>
+                <Navbar.Collapse>
+                    {collapseItems.map((item, index) => (
+                        <Navbar.CollapseItem key={item}>
+                            <Link
+                                color="inherit"
+                                css={{
+                                    minWidth: "100%",
+                                }}
+                                href="#"
+                            >
+                                {item}
+                            </Link>
+                        </Navbar.CollapseItem>
+                    ))}
+                </Navbar.Collapse>
+            </Navbar>
+        </Layout>
     );
 }
 
-export default Navbar;
+export default NavbarUI
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,8 @@
 import IconButton from './IconButton'
 import StyledBadge from './StyledBadge'
 import Notify from './Notify';
+import Layout from './Layout';
+import Box from './Box';
 import Navbar from './Nabvar';
 
-export { IconButton, StyledBadge, Notify, Navbar };
+export { IconButton, StyledBadge, Notify, Layout, Box, Navbar };
